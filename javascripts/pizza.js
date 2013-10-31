26a7c4796628cd77a163d6e31be0c70ff992b844
Jordan Humphreys <jordan@mailyard.net>
2013-10-30 22:22:37 -0700

    Add donut graph.

--- javascripts/pizza.js
+++ javascripts/pizza.js
@@ -7,6 +7,8 @@
     version : '1.0.0',
 
     settings : {
+      donut: false,
+      donut_inner_ratio: 0.4, // between 0 and 1
       percent_offset: 35, // relative to radius
       stroke_color: '#333',
       stroke_width: 0,
@@ -72,10 +74,6 @@
         var legend = $(this), graph;
         legend.data('settings', $.extend({}, self.settings, options, legend.data('options')));
         self.data(legend);
-
-        if (legend.data('bar-id')) {
-          return self.update_DOM(self.bar(legend));
-        }
 
         return self.update_DOM(self.pie(legend));
       });
@@ -171,8 +169,17 @@
         path.node.setAttribute('data-cx', cx);
         path.node.setAttribute('data-cy', cy);
 
+        if (settings.donut) {
+          this.annular_sector(path.node, {
+            centerX:cx, centerY:cy,
+            startDegrees:start_angle, endDegrees:end_angle,
+            innerRadius: (r * settings.donut_inner_ratio), outerRadius:r
+          });
+        } else {
+          path.attr({d:d});
+        }
+
         path.attr({
-          d: d,
           fill: data[i].color,
           stroke: settings.stroke_color,
           strokeWidth: settings.stroke_width
@@ -253,16 +260,69 @@
       return svg;
     },
 
+    // Options:
+    // - centerX, centerY: coordinates for the center of the circle    
+    // - startDegrees, endDegrees: fill between these angles, clockwise
+    // - innerRadius, outerRadius: distance from the center
+    // - thickness: distance between innerRadius and outerRadius
+    //   You should only specify two out of three of the radii and thickness
+    annular_sector : function (path, options) {
+      var opts = optionsWithDefaults(options);
+
+      // var x1 = cx + r * Math.sin(start_angle);
+      //   var y1 = cy - r * Math.cos(start_angle);
+      //   var x2 = cx + r * Math.sin(end_angle);
+      //   var y2 = cy - r * Math.cos(end_angle);
+      var p = [ // points
+        [opts.cx + opts.r2*Math.sin(opts.startRadians),
+         opts.cy - opts.r2*Math.cos(opts.startRadians)],
+        [opts.cx + opts.r2*Math.sin(opts.closeRadians),
+         opts.cy - opts.r2*Math.cos(opts.closeRadians)],
+        [opts.cx + opts.r1*Math.sin(opts.closeRadians),
+         opts.cy - opts.r1*Math.cos(opts.closeRadians)],
+        [opts.cx + opts.r1*Math.sin(opts.startRadians),
+         opts.cy - opts.r1*Math.cos(opts.startRadians)],
+      ];
+
+      var angleDiff = opts.closeRadians - opts.startRadians;
+      var largeArc = (angleDiff % (Math.PI*2)) > Math.PI ? 1 : 0;
+      var cmds = [];
+      cmds.push("M"+p[0].join());                                // Move to P0
+      cmds.push("A"+[opts.r2,opts.r2,0,largeArc,1,p[1]].join()); // Arc to  P1
+      cmds.push("L"+p[2].join());                                // Line to P2
+      cmds.push("A"+[opts.r1,opts.r1,0,largeArc,0,p[3]].join()); // Arc to  P3
+      cmds.push("z");                                // Close path (Line to P0)
+      path.setAttribute('d',cmds.join(' '));
+
+      function optionsWithDefaults(o){
+        // Create a new object so that we don't mutate the original
+        var o2 = {
+          cx           : o.centerX || 0,
+          cy           : o.centerY || 0,
+          startRadians : (o.startDegrees || 0),
+          closeRadians : (o.endDegrees   || 0),
+        };
+
+        var t = o.thickness!==undefined ? o.thickness : 100;
+        if (o.innerRadius!==undefined)      o2.r1 = o.innerRadius;
+        else if (o.outerRadius!==undefined) o2.r1 = o.outerRadius - t;
+        else                                o2.r1 = 200           - t;
+        if (o.outerRadius!==undefined)      o2.r2 = o.outerRadius;
+        else                                o2.r2 = o2.r1         + t;
+
+        if (o2.r1<0) o2.r1 = 0;
+        if (o2.r2<0) o2.r2 = 0;
+
+        return o2;
+      }
+    },
+
     identifier : function (legend) {
       if (legend.data('pie-id')) {
         return '#' + legend.data('pie-id');
       }
       
       return '#' + legend.data('bar-id');
-    },
-
-    reflow : function () {
-      this.build($('[data-pie-id]'));
     },
 
     throttle : function(fun, delay) {
